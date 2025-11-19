c003846edfd33f627944dfe9b989b0bd560fc265
fdesouzabcn <fdesouzabcn@gmail.com>
2025-11-19 15:52:51 +0100

    refactor:  Employee class with constructor and methods based on feedbacks received

---
+++ src/s1_04_n1_e1/s1_04_n1_e1.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+class Employee 
+{
+    private string $name;
+    private float $salary;
+    
+    public function __construct(string $name, float $salary) {
+        if ($salary < 0) {
+            throw new InvalidArgumentException("Salary must be greater than or equal to 0");
+        }
+
+        $this->name = $name;
+        $this->salary = $salary;
+    }
+    
+        public function isTaxable(): bool{
+        return $this->salary > 6000;
+    }
+
+    public function printTaxInfo(): void
+    {
+        $taxStatus = $this->isTaxable() ? "YES" : "NO";
+        
+        echo "Name: {$this->name}\n";
+        echo "Salary: {$this->salary}€\n";
+        echo "Pays Taxes: {$taxStatus}\n";
+        echo "-------------------\n";
+    }
+}
+
+?>
